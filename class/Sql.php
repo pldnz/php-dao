ca565ad9e4c30b22a928e6ae714d662e2573d970
PauloDiniz <paulo.diniz@visie.com.br>
2022-04-10 20:40:47 -0300

    iniciado projeto

---
+++ class/Sql.php
@@ -0,0 +1,31 @@
+<?php
+    class Sql extends PDO {
+        private $conn;
+
+        public function __construct() {
+            $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "paulo", "123456");
+        }
+
+        private function setParams($statement, $parameters = array()) {
+            foreach ($parameters as $key => $value) {
+                $this->setParam($statement, $key, $value);
+            }
+        }
+
+        private function setParam($statement, $key, $value) {
+            $statement->bindParam($key, $value);
+        }
+
+        public function execQuery($rawQuery, $params = array()) {  
+            $stmt = $this->conn->prepare($rawQuery);
+            $this->setParams($stmt, $params);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function select($rawQuery, $params = array()):array {
+            $stmt = $this->execQuery($rawQuery, $params);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+?>
