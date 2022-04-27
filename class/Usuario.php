dd93f428bd6fc1d21a9429c794488376a3b464f9
PauloDiniz <paulo.diniz@visie.com.br>
2022-04-26 22:09:01 -0300

    criado métodos update, insert e delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -43,21 +43,92 @@
                 ":ID"=>$id
             ));
 
-            if (count($results) > 0) {
-                $row = $results[0];
-                $this->setIdusuario($row['idusuario']);
-                $this->setDeslogin($row['deslogin']);
-                $this->setDessenha($row['dessenha']);
-                $this->setDtcadastro(new DateTime($row['dtcadastro'])); 
+            if (count($results) > 0) { 
+                $this->setData($results[0]); 
             }
         }
 
+        //como não estamos utilizando this, podemos transformar o método em estático
+        public static function getList() {
+            $sql = new Sql();
+            return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+        }
+
+        public static function search($login) {
+            $sql = new Sql();
+            return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+                ":SEARCH"=>"%".$login."%"
+            ));
+        }
+
+        public function login($login, $password) {
+            $sql = new Sql();
+            $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+                ":LOGIN"=>$login,
+                ":PASSWORD"=>$password
+            ));
+
+            if (count($results) > 0) {
+                $this->setData($results[0]); 
+            } else {
+                throw new Exception("Login e/ou senha inválidos.");
+            }
+        }
+
+        public function setData($data) {
+            $this->setIdusuario($data['idusuario']);
+            $this->setDeslogin($data['deslogin']);
+            $this->setDessenha($data['dessenha']);
+            $this->setDtcadastro(new DateTime($data['dtcadastro']));
+        }
+
+        public function insert() {
+            $sql = new Sql();
+            $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+                ":LOGIN"=>$this->getDeslogin(),
+                ":PASSWORD"=>$this->getDessenha()
+            ));
+
+            if (count($results) > 0) {
+                $this->setData($results[0]);
+            }
+        }
+
+        public function update($login, $password) {
+            $this->setDeslogin($login);
+            $this->setDessenha($password);
+
+            $sql = new Sql();
+            $sql->execQuery("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+                ":LOGIN"=>$this->getDeslogin(),
+                ":PASSWORD"=>$this->getDessenha(),
+                ":ID"=>$this->getIdusuario()
+            ));
+        }
+
+        public function delete() {
+            $sql = new Sql();
+            $sql->execQuery("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+                ":ID"=>$this->getIdusuario()
+            ));
+
+            $this->setIdusuario(0);
+            $this->setDeslogin("");
+            $this->setDessenha("");
+            $this->setDtcadastro(new DateTime());
+        }
+
+        public function __construct($login = "", $password = "") {
+            $this->setDeslogin($login);
+            $this->setDessenha($password);
+        }
+ 
         public function __toString() {
             return json_encode(array(
                 "idusuario"=>$this->getIdusuario(),
                 "deslogin"=>$this->getDeslogin(),
                 "dessenha"=>$this->getDessenha(),
-                "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+                "dtcadastro"=>$this->getDtcadastro()
             ));
         }
     }
--- index.php
+++ index.php
@@ -1,6 +1,39 @@
 <?php
     require_once("config.php");
+    
+    // $usuario = new Usuario();
+    // $usuario->loadById(2);
+    // echo $usuario;
+    // echo '<br>';
+
+
+    //utilizando método estático
+    // $lista = Usuario::getList();
+    // echo json_encode($lista);
+
+
+    // $search = Usuario::search("benzema");
+    // echo json_encode($search);
+
+
+    // $usuario = new Usuario();
+    // $usuario->login("ronaldo", "cr7");
+    // echo $usuario;
+
+
+    // $jogador = new Usuario("Rony", "pintounotificacao");
+    // $jogador->insert();
+    // echo $jogador;
+
+
+    // $usuario = new Usuario();
+    // $usuario->loadById(7);
+    // $usuario->update("vini jr", "malvadeza");
+    // echo $usuario;
+
+
     $usuario = new Usuario();
-    $usuario->loadById(2);
+    $usuario->loadById(6);
+    $usuario->delete();
     echo $usuario;
 ?>
